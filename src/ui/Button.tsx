591bbbdd8f37ba531631b37678179e03c58437af
Shaw <minjongbaek@gmail.com>
2023-10-03 01:45:56 +0900

    프로젝트 초기 설정 (#1)
    * feat: initialize vite, react, and tailwind
    
    * feat: initialize storybook
    
    * docs: create PR template
    
    * chore: migrate yarn to npm
    
    * chore: add prettier

---
+++ src/ui/Button.tsx
@@ -0,0 +1,13 @@
+import { ComponentPropsWithoutRef } from 'react';
+
+type ButtonProps = ComponentPropsWithoutRef<'button'>;
+
+const Button = ({ children, ...props }: ButtonProps) => {
+  return (
+    <button type='button' {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
