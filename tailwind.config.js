afd0ccae9cc3a3f379ef24f6e89d244b5a087222
minjongbaek <minjongbaek@gmail.com>
2023-10-29 16:00:27 +0900

    chore: tailwind fontSize 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
+    fontSize: {
+      xs: ['8px', '16px'],
+      sm: ['12px', '20px'],
+      md: ['16px', '24px'],
+      lg: ['20px', '28px'],
+    },
     extend: {},
   },
   plugins: [],
