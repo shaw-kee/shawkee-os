78d4eeca81b726e36b959bb310d396ca2f99961b
minjongbaek <minjongbaek@gmail.com>
2023-11-03 23:40:44 +0900

    레이아웃 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <div className='w-full h-screen'>
+      {/* NavBar */}
+      {/* Desktop */}
+      {/* Dock */}
+    </div>
+  );
 }
 
 export default App;
