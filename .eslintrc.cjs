3f51133b34a621c53f87a602f4ab3fdd27eb6d01
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2023-11-03 23:27:09 +0900

    chore: storybook 관련 패키지 제거

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:storybook/recommended',
     'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
