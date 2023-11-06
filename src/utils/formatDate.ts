25e1b4c7d5e852eb2c30dc073984f4b2a46e75b9
minjongbaek <minjongbaek@gmail.com>
2023-11-06 22:02:59 +0900

    refactor: formatDate 함수를 유틸 함수로 분리

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,16 @@
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+export const formatDate = (date: Date) => {
+  const dayOfWeek = days[date.getDay()];
+  const month = months[date.getMonth()];
+  const day = date.getDate();
+  let hour = date.getHours();
+  const minute = date.getMinutes();
+  const meridiem = hour >= 12 ? 'PM' : 'AM';
+
+  hour = hour % 12;
+  hour = hour === 0 ? 12 : hour;
+  const minutePadded = minute < 10 ? '0' + minute : minute;
+  return `${dayOfWeek} ${month} ${day}  ${hour}:${minutePadded} ${meridiem}`;
+};
