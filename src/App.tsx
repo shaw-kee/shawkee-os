67081714e99c5bbe784fd8a849ec653613af9e46
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2023-11-10 01:06:24 +0900

    feat: Docs Wrapper UI 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import MenuBar from './components/MenuBar';
 import WallpaperUrl from './assets/wallpaper.jpg';
+import Docs from '@/components/Docs';
 
 function App() {
   return (
     <div className='w-full h-screen'>
       <img src={WallpaperUrl} className='absolute w-full h-full object-cover' />
       <MenuBar />
-      {/* Desktop */}
-      {/* Dock */}
+      <Docs />
     </div>
   );
 }
