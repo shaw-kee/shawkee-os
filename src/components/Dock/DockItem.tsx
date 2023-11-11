c57c0f2ecc4fdc13d966784f004fc1b0e57a2c43
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2023-11-12 02:43:49 +0900

    design: DockItem style 변경 및 이미지 리소스 변경

--- src/components/Dock/DockItem.tsx
+++ src/components/Dock/DockItem.tsx
@@ -9,12 +9,12 @@ interface Props {
 
 const DockItem = ({ title, imageUrl, isOpen = false, link = '' }: Props) => {
   return (
-    <div className='tooltip flex flex-col w-12 h-full items-center hover:cursor-pointer'>
+    <div className='tooltip flex flex-col w-12 items-center justify-start hover:cursor-pointer'>
       <ConditionalLink link={link}>
         <div className='tooltip-text top-[-50%] inline-flex items-center rounded-md px-3 py-1 text-sm text-black bg-[#F6F6F6B8] shadow-[0_2px_6px_0_rgba(0,0,0,0.20),0_0_0_0.5px_rgba(0,0,0,0.12)]'>
           {title}
         </div>
-        <img src={imageUrl} className='w-12 h-12' />
+        <img src={imageUrl} className='w-12 aspect-square' />
         <div className={`w-1 h-1 rounded-full bg-[#575757] ${isOpen ? '' : 'hidden'}`} />
       </ConditionalLink>
     </div>
--- src/components/Dock/index.tsx
+++ src/components/Dock/index.tsx
@@ -4,7 +4,7 @@ import DockItem from '@/components/Dock/DockItem';
 const Dock = () => {
   return (
     <>
-      <div className='flex gap-1 absolute p-1 bottom-[5px] left-[50%] translate-x-[-50%] bg-[#F6F6F6]/[0.36] border rounded-2xl border-[#f3f3f323] shadow-[0_0_6px_0_rgba(0,0,0,0.15)] backdrop-blur-[68px]'>
+      <div className='flex gap-1 h-[65px] absolute p-1 bottom-[5px] left-[50%] translate-x-[-50%] bg-[#F6F6F6]/[0.36] border rounded-2xl border-[#f3f3f323] shadow-[0_0_6px_0_rgba(0,0,0,0.15)] backdrop-blur-[68px]'>
         {apps.map((app) => (
           <DockItem key={app.id} title={app.title} imageUrl={app.imageUrl} link={app.link} />
         ))}
