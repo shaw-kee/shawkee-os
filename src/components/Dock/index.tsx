b65b5d480e0f493e113daa00f031aa69c84fab16
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2023-11-11 18:09:52 +0900

    rename: 컴포넌트명 수정

--- src/components/Dock/index.tsx
+++ src/components/Dock/index.tsx
@@ -1,4 +1,4 @@
-const Docs = () => {
+const Dock = () => {
   return (
     <>
       <div className='absolute bottom-[5px] left-[50%] translate-x-[-50%] w-[400px] h-[65px] bg-[#F6F6F6] opacity-[0.36] border rounded-2xl border-[#1A1A1A]/[.46] shadow-[0_0_6px_0_rgba(0,0,0,0.15)] mix-blend-luminosity backdrop-blur-[68px]'></div>
@@ -6,4 +6,4 @@ const Docs = () => {
   );
 };
 
-export default Docs;
+export default Dock;
