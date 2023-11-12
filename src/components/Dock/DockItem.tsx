d6ec608948d277085bb5eb59cd756ac48917982b
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2023-11-12 22:21:47 +0900

    design: 툴팁 위치 변경

--- src/components/Dock/DockItem.tsx
+++ src/components/Dock/DockItem.tsx
@@ -26,7 +26,7 @@ const DockItem = ({ title, imageUrl, isOpen = false, link = '', mousePosition }:
       className='tooltip flex flex-col items-center justify-end hover:cursor-pointer'
     >
       <ConditionalLink link={link}>
-        <div className='tooltip-text top-[-100%] inline-flex items-center rounded-md px-3 py-1 text-sm text-black bg-[#F6F6F6B8] shadow-[0_2px_6px_0_rgba(0,0,0,0.20),0_0_0_0.5px_rgba(0,0,0,0.12)]'>
+        <div className='tooltip-text top-[calc(-100%-10px)] inline-flex items-center rounded-md px-3 py-1 text-sm text-black bg-[#F6F6F6B8] shadow-[0_2px_6px_0_rgba(0,0,0,0.20),0_0_0_0.5px_rgba(0,0,0,0.12)]'>
           {title}
         </div>
         <img src={imageUrl} className='w-full aspect-square' />
