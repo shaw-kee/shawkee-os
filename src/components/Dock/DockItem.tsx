b15a378985779864d90f28bea7fd97e9f0b1bc24
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2023-11-12 22:18:14 +0900

    feat: Dock 마우스 이동 애니메이션 구현

--- src/components/Dock/DockItem.tsx
+++ src/components/Dock/DockItem.tsx
@@ -1,23 +1,38 @@
 import ConditionalLink from '@/components/ConditionalLink';
+import { useSpring, useTransform, motion, MotionValue } from 'framer-motion';
+import { useRef } from 'react';
 
 interface Props {
   title: string;
   imageUrl: string;
   isOpen?: boolean;
   link?: string;
+  mousePosition: MotionValue;
 }
 
-const DockItem = ({ title, imageUrl, isOpen = false, link = '' }: Props) => {
+const DockItem = ({ title, imageUrl, isOpen = false, link = '', mousePosition }: Props) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const distance = useTransform(mousePosition, (value) => {
+    const itemPosition = ref.current?.getBoundingClientRect() ?? { x: 0, width: 0 };
+    return value - itemPosition.x - itemPosition.width / 2;
+  });
+  const widthSync = useTransform(distance, [-200, 0, 200], [50, 100, 50]);
+  const width = useSpring(widthSync, { mass: 0.2, stiffness: 150, damping: 10 });
+
   return (
-    <div className='tooltip flex flex-col w-12 items-center justify-start hover:cursor-pointer'>
+    <motion.div
+      ref={ref}
+      style={{ width }}
+      className='tooltip flex flex-col items-center justify-end hover:cursor-pointer'
+    >
       <ConditionalLink link={link}>
-        <div className='tooltip-text top-[-50%] inline-flex items-center rounded-md px-3 py-1 text-sm text-black bg-[#F6F6F6B8] shadow-[0_2px_6px_0_rgba(0,0,0,0.20),0_0_0_0.5px_rgba(0,0,0,0.12)]'>
+        <div className='tooltip-text top-[-100%] inline-flex items-center rounded-md px-3 py-1 text-sm text-black bg-[#F6F6F6B8] shadow-[0_2px_6px_0_rgba(0,0,0,0.20),0_0_0_0.5px_rgba(0,0,0,0.12)]'>
           {title}
         </div>
-        <img src={imageUrl} className='w-12 aspect-square' />
-        <div className={`w-1 h-1 rounded-full bg-[#575757] ${isOpen ? '' : 'hidden'}`} />
+        <img src={imageUrl} className='w-full aspect-square' />
+        <div className={`w-1 h-1 absolute bottom-[2px] rounded-full bg-[#575757] ${isOpen ? '' : 'hidden'}`} />
       </ConditionalLink>
-    </div>
+    </motion.div>
   );
 };
 
--- src/components/Dock/index.tsx
+++ src/components/Dock/index.tsx
@@ -1,12 +1,27 @@
 import apps from '@/config/apps';
 import DockItem from '@/components/Dock/DockItem';
+import { useMotionValue } from 'framer-motion';
 
 const Dock = () => {
+  const mousePosition = useMotionValue(Infinity);
+  const handleMouseMove = (e: React.MouseEvent) => mousePosition.set(e.clientX);
+  const handleMouseLeave = () => mousePosition.set(Infinity);
+
   return (
     <>
-      <div className='flex gap-1 h-[65px] absolute p-1 bottom-[5px] left-[50%] translate-x-[-50%] bg-[#F6F6F6]/[0.36] border rounded-2xl border-[#f3f3f323] shadow-[0_0_6px_0_rgba(0,0,0,0.15)] backdrop-blur-[68px]'>
+      <div
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+        className='flex gap-1 h-[60px] absolute p-1 bottom-[5px] left-[50%] translate-x-[-50%] bg-[#F6F6F6]/[0.36] border rounded-2xl border-[#f3f3f323] shadow-[0_0_6px_0_rgba(0,0,0,0.15)] backdrop-blur-[68px]'
+      >
         {apps.map((app) => (
-          <DockItem key={app.id} title={app.title} imageUrl={app.imageUrl} link={app.link} />
+          <DockItem
+            key={app.id}
+            title={app.title}
+            imageUrl={app.imageUrl}
+            link={app.link}
+            mousePosition={mousePosition}
+          />
         ))}
       </div>
     </>
