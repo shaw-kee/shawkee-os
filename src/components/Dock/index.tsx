ddea842139d97a45c9155929940bfaf73e1ab8aa
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2023-11-12 23:44:13 +0900

    fix: 불필요한 fragment 제거

--- src/components/Dock/index.tsx
+++ src/components/Dock/index.tsx
@@ -9,24 +9,22 @@ const Dock = () => {
   const handleMouseLeave = () => mousePosition.set(Infinity);
 
   return (
-    <>
-      <div
-        onMouseMove={handleMouseMove}
-        onMouseLeave={handleMouseLeave}
-        className='flex gap-1 absolute p-1 bottom-[5px] left-[50%] translate-x-[-50%] bg-[#F6F6F6]/[0.36] border rounded-2xl border-[#f3f3f323] shadow-[0_0_6px_0_rgba(0,0,0,0.15)] backdrop-blur-[68px]'
-        style={{ height: DOCK_SIZE + 10 }}
-      >
-        {apps.map((app) => (
-          <DockItem
-            key={app.id}
-            title={app.title}
-            imageUrl={app.imageUrl}
-            link={app.link}
-            mousePosition={mousePosition}
-          />
-        ))}
-      </div>
-    </>
+    <div
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      className='flex gap-1 absolute p-1 bottom-[5px] left-[50%] translate-x-[-50%] bg-[#F6F6F6]/[0.36] border rounded-2xl border-[#f3f3f323] shadow-[0_0_6px_0_rgba(0,0,0,0.15)] backdrop-blur-[68px]'
+      style={{ height: DOCK_SIZE + 10 }}
+    >
+      {apps.map((app) => (
+        <DockItem
+          key={app.id}
+          title={app.title}
+          imageUrl={app.imageUrl}
+          link={app.link}
+          mousePosition={mousePosition}
+        />
+      ))}
+    </div>
   );
 };
 
