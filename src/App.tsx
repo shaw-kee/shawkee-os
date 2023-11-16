e82df353afa4444e88d84c887f320c1cdfa60251
jaeung-E <jaeung5169@naver.com>
2023-11-16 18:18:20 +0900

    feat: AppProvider 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import MenuBar from './components/MenuBar';
 import WallpaperUrl from './assets/wallpaper.jpg';
 import Dock from '@/components/Dock';
+import AppProvider from '@/store/App/AppProvider';
 
 function App() {
   return (
-    <div className='h-screen w-full'>
-      <img src={WallpaperUrl} className='absolute h-full w-full object-cover' />
-      <MenuBar />
-      <Dock />
-    </div>
+    <AppProvider>
+      <div className='h-screen w-full'>
+        <img src={WallpaperUrl} className='absolute h-full w-full object-cover' />
+        <MenuBar />
+        <Dock />
+      </div>
+    </AppProvider>
   );
 }
 
