53c56b2182bf9af555f4897fc8e8d24e8fdda93f
jaeung-E <jaeung5169@naver.com>
2023-11-16 12:21:08 +0900

    style: tailwind CSS prettier plugin에 의한 className 순서 변경

--- src/components/Dock/DockItem.tsx
+++ src/components/Dock/DockItem.tsx
@@ -22,11 +22,11 @@ const DockItem = ({ title, imageUrl, isOpen = false, link = '', mousePosition }:
       className='tooltip flex flex-col items-center justify-end hover:cursor-pointer'
     >
       <ConditionalLink link={link}>
-        <div className='tooltip-text top-[calc(-100%-10px)] inline-flex items-center rounded-md px-3 py-1 text-sm text-black bg-[#F6F6F6B8] shadow-[0_2px_6px_0_rgba(0,0,0,0.20),0_0_0_0.5px_rgba(0,0,0,0.12)]'>
+        <div className='tooltip-text top-[calc(-100%-10px)] inline-flex items-center rounded-md bg-[#F6F6F6B8] px-3 py-1 text-sm text-black shadow-[0_2px_6px_0_rgba(0,0,0,0.20),0_0_0_0.5px_rgba(0,0,0,0.12)]'>
           {title}
         </div>
-        <img src={imageUrl} className='w-full aspect-square' />
-        <div className={`w-1 h-1 absolute bottom-[2px] rounded-full bg-[#575757] ${isOpen ? '' : 'hidden'}`} />
+        <img src={imageUrl} className='aspect-square w-full' />
+        <div className={`absolute bottom-[2px] h-1 w-1 rounded-full bg-[#575757] ${isOpen ? '' : 'hidden'}`} />
       </ConditionalLink>
     </motion.div>
   );
--- src/components/Dock/index.tsx
+++ src/components/Dock/index.tsx
@@ -12,7 +12,7 @@ const Dock = () => {
     <div
       onMouseMove={handleMouseMove}
       onMouseLeave={handleMouseLeave}
-      className='flex gap-1 absolute p-1 bottom-[5px] left-[50%] translate-x-[-50%] bg-[#F6F6F6]/[0.36] border rounded-2xl border-[#f3f3f323] shadow-[0_0_6px_0_rgba(0,0,0,0.15)] backdrop-blur-[68px]'
+      className='absolute bottom-[5px] left-[50%] flex translate-x-[-50%] gap-1 rounded-2xl border border-[#f3f3f323] bg-[#F6F6F6]/[0.36] p-1 shadow-[0_0_6px_0_rgba(0,0,0,0.15)] backdrop-blur-[68px]'
       style={{ height: DOCK_SIZE + 10 }}
     >
       {apps.map((app) => (
