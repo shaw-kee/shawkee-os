27b5ec3b66db5e3b0b2d9fe1f2d17f08c622653b
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2023-11-25 22:50:36 +0900

    feat: DockItem App 열기 이벤트 추가

--- src/components/Dock/DockItem.tsx
+++ src/components/Dock/DockItem.tsx
@@ -13,18 +13,23 @@ interface Props {
   mousePosition: MotionValue;
 }
 
-const DockItem = ({ title, imageUrl, isOpen = false, link = '', mousePosition }: Props) => {
+const DockItem = ({ id, title, imageUrl, isOpen = false, link = '', mousePosition }: Props) => {
   const dispatch = useContext(AppReducerContext);
   const ref = useRef<HTMLDivElement>(null);
   const width = useCalculatedWidth(mousePosition, ref);
 
   if (!dispatch) throw new Error('dispatch is null');
 
+  const handleClick = () => {
+    dispatch({ type: 'OPEN', id });
+  };
+
   return (
     <motion.div
       ref={ref}
       style={{ width }}
       className='tooltip flex flex-col items-center justify-end hover:cursor-pointer'
+      onClick={handleClick}
     >
       <ConditionalLink link={link}>
         <div className='tooltip-text top-[calc(-100%-10px)] inline-flex items-center rounded-md bg-[#F6F6F6B8] px-3 py-1 text-sm text-black shadow-[0_2px_6px_0_rgba(0,0,0,0.20),0_0_0_0.5px_rgba(0,0,0,0.12)]'>
