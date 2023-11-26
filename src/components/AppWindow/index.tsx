5db104bc93454c2116e67d4bd59f9b0d9e841ab5
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2023-11-26 13:50:31 +0900

    refactor: 익명 함수를 함수 표현식으로 변경

--- src/components/AppWindow/index.tsx
+++ src/components/AppWindow/index.tsx
@@ -11,13 +11,15 @@ const AppWindow = ({ title, id }: Props) => {
 
   if (!dispatch) throw new Error('dispatch is null');
 
+  const handleClose = () => dispatch({ type: 'CLOSE', id });
+
   return (
     <div className='absolute left-[50%] top-[50%] w-96 translate-x-[-50%] translate-y-[-50%] flex-col overflow-hidden rounded-lg'>
       <div className='flex h-7 w-full items-center justify-center bg-[#e4e4e4]'>
         <div className='absolute left-2 flex items-center gap-1'>
           <div
             className='h-3 w-3 cursor-pointer rounded-full border-[0.5px] border-[#00000033] bg-[#FF5F57]'
-            onClick={() => dispatch({ type: 'CLOSE', id })}
+            onClick={handleClose}
           />
           <div className='h-3 w-3 cursor-pointer rounded-full border-[0.5px] border-[#00000033] bg-[#FEBC2E]' />
           <div className='h-3 w-3 cursor-pointer rounded-full border-[0.5px] border-[#00000033] bg-[#28C840]' />
