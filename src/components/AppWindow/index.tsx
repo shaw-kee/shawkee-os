2d3ebb7d5b49d2bb25018cadaf96f0b3e828e4ce
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2023-11-26 16:36:45 +0900

    design: AppWindow 컨트롤 버튼 아이콘 hidden 처리

--- src/components/AppWindow/index.tsx
+++ src/components/AppWindow/index.tsx
@@ -19,18 +19,18 @@ const AppWindow = ({ title, id }: Props) => {
   return (
     <div className='absolute left-[50%] top-[50%] w-96 translate-x-[-50%] translate-y-[-50%] flex-col overflow-hidden rounded-lg'>
       <div className='flex h-7 w-full items-center justify-center bg-[#e4e4e4]'>
-        <div className='absolute left-2 flex items-center gap-1'>
+        <div className='hidden-wrapper absolute left-2 flex items-center gap-1'>
           <button
             className='flex h-3 w-3 cursor-pointer items-center justify-center rounded-full border-[0.5px] border-[#00000033] bg-[#FF5F57]'
             onClick={handleClose}
           >
-            <CloseIcon width={6} height={6} viewBox='0 0 12 12' color='#A21B2B' />
+            <CloseIcon width={6} height={6} viewBox='0 0 12 12' color='#A21B2B' className='hidden' />
           </button>
           <button className='flex h-3 w-3 cursor-pointer items-center justify-center rounded-full border-[0.5px] border-[#00000033] bg-[#FEBC2E]'>
-            <MinimizeIcon width={6} height={6} viewBox='0 0 12 12' color='#96550F' />
+            <MinimizeIcon width={6} height={6} viewBox='0 0 12 12' color='#96550F' className='hidden' />
           </button>
           <button className='flex h-3 w-3 cursor-pointer items-center justify-center rounded-full border-[0.5px] border-[#00000033] bg-[#28C840]'>
-            <FullScreenIcon width={6} height={6} viewBox='0 0 12 12' color='#137006' />
+            <FullScreenIcon width={6} height={6} viewBox='0 0 12 12' color='#137006' className='hidden' />
           </button>
         </div>
         <span className='font-bold'>{title}</span>
