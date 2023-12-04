defeff49b063d45c65855ed33a85d2f15bb4d04f
minjongbaek <minjongbaek@gmail.com>
2023-12-04 23:17:07 +0900

    refactor: MenuList 관련 상태를 끌어올리기

--- src/components/MenuBar/MenuBarItem.tsx
+++ src/components/MenuBar/MenuBarItem.tsx
@@ -1,34 +1,25 @@
 import type { ReactNode } from 'react';
 import { Menu } from '@/types/menu';
-import MenuList from './MenuList';
-import useToggleMenuList from './useToggleMenuList';
 
 interface MenuBarItemProps {
   isAppName?: boolean;
   children: ReactNode;
   menus?: Menu[];
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const MenuBarItem = ({ isAppName = false, children, menus }: MenuBarItemProps) => {
+const MenuBarItem = ({ isAppName = false, children, onClick }: MenuBarItemProps) => {
   const fontWeightClass = isAppName ? 'font-bold' : 'font-base';
-
-  const { menuListRef, isOpen, menuListPosition, toggleMenuList } = useToggleMenuList();
-
-  const handleMenuBarItemClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    const rect = event.currentTarget.getBoundingClientRect();
-    toggleMenuList(rect);
-  };
 
   return (
     <>
       <button
         type='button'
         className={`text cursor-default rounded-[4px] px-[11px] py-[4px] text-[13px] leading-[16px] active:bg-[#090909]/10 ${fontWeightClass}`}
-        onClick={handleMenuBarItemClick}
+        onClick={onClick}
       >
         {children}
       </button>
-      {menus && isOpen && <MenuList ref={menuListRef} position={menuListPosition} menus={menus} />}
     </>
   );
 };
--- src/components/MenuBar/index.tsx
+++ src/components/MenuBar/index.tsx
@@ -4,35 +4,47 @@ import SpotlightIcon from '@/assets/icons/SpotlightIcon.svg?react';
 import WifiIcon from '@/assets/icons/WifiIcon.svg?react';
 import MenuBarItem from './MenuBarItem';
 import Clock from './Clock';
+import useToggleMenuList from './useToggleMenuList';
+import MenuList from './MenuList';
 
 const DEFAULT_MENUS = ['Menu1', 'Menu2', 'Menu3', 'LongTextMenuLongTextMenuLongTextMenu'];
 
 const MenuBar = () => {
+  const { menuListRef, isOpen, menuListPosition, toggleMenuList } = useToggleMenuList();
+
+  const handleMenuBarItemClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const rect = event.currentTarget.getBoundingClientRect();
+    toggleMenuList(rect);
+  };
+
   return (
-    <div className='flex w-full justify-between bg-white/50 px-[8px] backdrop-blur-[25px]'>
-      <div className='flex'>
-        <MenuBarItem menus={DEFAULT_MENUS}>
-          <AppleIcon width={14} height={17} viewBox='0 0 14 17' />
-        </MenuBarItem>
-        <MenuBarItem isAppName>Finder</MenuBarItem>
-        <MenuBarItem>Menu1</MenuBarItem>
-        <MenuBarItem menus={DEFAULT_MENUS}>Menu2</MenuBarItem>
+    <>
+      <div className='flex w-full justify-between bg-white/50 px-[8px] backdrop-blur-[25px]'>
+        <div className='flex'>
+          <MenuBarItem onClick={handleMenuBarItemClick}>
+            <AppleIcon width={14} height={17} viewBox='0 0 14 17' />
+          </MenuBarItem>
+          <MenuBarItem isAppName>Finder</MenuBarItem>
+          <MenuBarItem>Menu1</MenuBarItem>
+          <MenuBarItem onClick={handleMenuBarItemClick}>Menu2</MenuBarItem>
+        </div>
+        <div className='flex'>
+          <MenuBarItem>
+            <WifiIcon width={16} height={11} viewBox='0 0 16 11' />
+          </MenuBarItem>
+          <MenuBarItem>
+            <SpotlightIcon width={14} height={13} viewBox='0 0 14 13' />
+          </MenuBarItem>
+          <MenuBarItem>
+            <ControlCenterIcon width={14} height={13} viewBox='0 0 14 13' />
+          </MenuBarItem>
+          <MenuBarItem onClick={handleMenuBarItemClick}>
+            <Clock />
+          </MenuBarItem>
+        </div>
       </div>
-      <div className='flex'>
-        <MenuBarItem>
-          <WifiIcon width={16} height={11} viewBox='0 0 16 11' />
-        </MenuBarItem>
-        <MenuBarItem>
-          <SpotlightIcon width={14} height={13} viewBox='0 0 14 13' />
-        </MenuBarItem>
-        <MenuBarItem>
-          <ControlCenterIcon width={14} height={13} viewBox='0 0 14 13' />
-        </MenuBarItem>
-        <MenuBarItem menus={DEFAULT_MENUS}>
-          <Clock />
-        </MenuBarItem>
-      </div>
-    </div>
+      {isOpen && <MenuList ref={menuListRef} position={menuListPosition} menus={DEFAULT_MENUS} />}
+    </>
   );
 };
 
