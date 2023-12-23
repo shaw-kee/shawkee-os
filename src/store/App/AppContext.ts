656e206216c24de0dc42ced6eb3d898d79933382
jaeungE <jaeung5169@naver.com>
2023-12-23 18:27:45 +0900

    feat: AppWindow z-index 최대값 및 초기화 기능 추가

--- src/store/App/AppContext.ts
+++ src/store/App/AppContext.ts
@@ -1,5 +1,6 @@
 import { Dispatch, createContext } from 'react';
 import { apps, App } from '@/config/apps';
+import { INITIAL_INDEX, MAXIMUM_INDEX } from '@/constants/app';
 
 interface ActionType {
   type: 'CLOSE' | 'OPEN';
@@ -11,6 +12,11 @@ export const AppReducerContext = createContext<Dispatch<ActionType> | null>(null
 
 export const appReducer = (state: App[], action: ActionType): App[] => {
   const maxIndex = Math.max(...state.map((app) => app.zIndex));
+  if (maxIndex >= MAXIMUM_INDEX) {
+    return state.map((app) =>
+      app.id === action.id ? { ...app, zIndex: INITIAL_INDEX + 1 } : { ...app, zIndex: INITIAL_INDEX }
+    );
+  }
 
   switch (action.type) {
     case 'CLOSE':
