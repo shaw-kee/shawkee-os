112bb5a1f208048999bec54c0ca494d14767c0cb
jaeungE <jaeung5169@naver.com>
2024-01-19 04:39:36 +0900

    feat: AppWindow 전체화면 기능 구현

--- src/components/AppWindow/ControlBox.tsx
+++ src/components/AppWindow/ControlBox.tsx
@@ -1,31 +1,50 @@
 import CloseIcon from '@/assets/icons/AppWindow/Close.svg?react';
 import MinimizeIcon from '@/assets/icons/AppWindow/Minimize.svg?react';
-import FullScreenIcon from '@/assets/icons/AppWindow/FullScreen.svg?react';
+import FullscreenIcon from '@/assets/icons/AppWindow/Fullscreen.svg?react';
+import ExitFullscreenIcon from '@/assets/icons/AppWindow/ExitFullscreen.svg?react';
 import { MouseEvent } from 'react';
 
 interface Props {
+  isFullscreen: boolean;
   handleClose: () => void;
   handleMinimize: (e: MouseEvent) => void;
+  handleFullscreen: () => void;
 }
 
-const ControlBox = ({ handleClose, handleMinimize }: Props) => {
+const ControlBox = ({ handleClose, handleMinimize, handleFullscreen, isFullscreen }: Props) => {
   return (
     <div className='hidden-wrapper absolute left-2 flex items-center gap-1'>
       <button
         className='flex h-3 w-3 cursor-pointer items-center justify-center rounded-full border-[0.5px] border-[#00000033] bg-[#FF5F57]'
         onClick={handleClose}
       >
-        <CloseIcon width={6} height={6} viewBox='0 0 12 12' color='#A21B2B' className='hidden' />
+        <CloseIcon width={6} height={6} viewBox='0 0 12 12' color='#A21B2B' className='hidden-item hidden' />
       </button>
       <button
-        className='flex h-3 w-3 cursor-pointer items-center justify-center 
-          rounded-full border-[0.5px] border-[#00000033] bg-[#FEBC2E]'
+        className={`flex h-3 w-3 ${isFullscreen ? 'bg-[#9ca3af]' : 'cursor-pointer bg-[#FEBC2E]'} items-center 
+          justify-center rounded-full border-[0.5px] border-[#00000033]`}
         onClick={handleMinimize}
+        disabled={isFullscreen}
       >
-        <MinimizeIcon width={6} height={6} viewBox='0 0 12 12' color='#96550F' className='hidden' />
+        {!isFullscreen && (
+          <MinimizeIcon width={6} height={6} viewBox='0 0 12 12' color='#96550F' className='hidden-item hidden' />
+        )}
       </button>
-      <button className='flex h-3 w-3 cursor-pointer items-center justify-center rounded-full border-[0.5px] border-[#00000033] bg-[#28C840]'>
-        <FullScreenIcon width={6} height={6} viewBox='0 0 12 12' color='#137006' className='hidden' />
+      <button
+        className='flex h-3 w-3 cursor-pointer items-center justify-center rounded-full border-[0.5px] border-[#00000033] bg-[#28C840]'
+        onClick={handleFullscreen}
+      >
+        {isFullscreen ? (
+          <ExitFullscreenIcon
+            width={10}
+            height={10}
+            viewBox='0 0 19 19'
+            color='#137006'
+            className='hidden-item hidden'
+          />
+        ) : (
+          <FullscreenIcon width={6} height={6} viewBox='0 0 12 12' color='#137006' className='hidden-item hidden' />
+        )}
       </button>
     </div>
   );
--- src/components/AppWindow/index.tsx
+++ src/components/AppWindow/index.tsx
@@ -7,6 +7,7 @@ import { DOCK_SIZE } from '@/constants/dock';
 import ControlBox from './ControlBox';
 import usePrevState from '@/hooks/usePrevState';
 
+const MENUBAR_HEIGHT = 25;
 interface Props {
   title: string;
   id: string;
@@ -48,10 +49,13 @@ const AppWindow = ({
     handleDragElement,
   } = useRND(initialPosition, minSize, boundary);
   const [isMaximize, setIsMaximize] = useState<boolean>(false);
+  const [isFullscreen, setIsFullscreen] = useState<boolean>(false);
   const [tempMinimize, setTempMinimize] = useState<Position & Size>({ x: 0, y: 0, width: 0, height: 0 });
   const [tempMaximize, setTempMaximize] = useState<Position & Size>({ x: 0, y: 0, width: 0, height: 0 });
+  const [tempFullscreen, setTempFullscreen] = useState<Position & Size>({ x: 0, y: 0, width: 0, height: 0 });
+  const titleBarRef = useRef<HTMLDivElement>(null);
   const windowRef = useRef<HTMLDivElement>(null);
-  const prevState = usePrevState({ isMinimize, isMaximize });
+  const prevState = usePrevState({ isMinimize, isMaximize, isFullscreen });
 
   useEffect(() => {
     if (!windowRef.current) return;
@@ -59,15 +63,27 @@ const AppWindow = ({
     let { x, y, width, height } = { x: 0, y: 0, width: 0, height: 0 };
     const minimize = !isMinimize && prevState.isMinimize;
     const maximize = !isMaximize && prevState.isMaximize;
+    const fullscreen = !isFullscreen && prevState.isFullscreen;
 
     if (minimize) ({ x, y, width, height } = tempMinimize);
     if (maximize) ({ x, y, width, height } = tempMaximize);
-    if (minimize || maximize) {
+    if (fullscreen) ({ x, y, width, height } = tempFullscreen);
+    if (minimize || maximize || fullscreen) {
       windowRef.current.style.transition = 'all 0.5s cubic-bezier(0.4, 0, 0.2, 1)';
       setPosition({ x, y });
       setSize({ width, height });
     }
-  }, [setPosition, setSize, tempMinimize, isMinimize, isMaximize, prevState, tempMaximize]);
+  }, [
+    setPosition,
+    setSize,
+    tempMinimize,
+    isMinimize,
+    isMaximize,
+    prevState,
+    tempMaximize,
+    isFullscreen,
+    tempFullscreen,
+  ]);
 
   const handleClose = () => dispatch({ type: 'CLOSE', id });
   const handleClickWindow = () => dispatch({ type: 'OPEN', id });
@@ -95,6 +111,17 @@ const AppWindow = ({
     }
   };
 
+  const handleFullscreen = () => {
+    if (windowRef.current && !isFullscreen) {
+      windowRef.current.style.transition = 'all 0.5s cubic-bezier(0.4, 0, 0.2, 1)';
+      setTempFullscreen({ x, y, width, height });
+      setPosition({ x: 0, y: -MENUBAR_HEIGHT });
+      setSize({ width: boundary.width, height: boundary.height + MENUBAR_HEIGHT });
+    }
+
+    setIsFullscreen((prev) => !prev);
+  };
+
   const handleTransitionEnd = () => {
     if (windowRef.current) windowRef.current.style.transition = 'none';
   };
@@ -102,40 +129,54 @@ const AppWindow = ({
   return (
     <div
       style={{ width, height, transform: `translate(${x}px, ${y}px)`, zIndex }}
-      className={`absolute flex flex-col overflow-hidden rounded-md ${isMinimize ? 'invisible opacity-0' : ''} `}
+      className={`absolute flex flex-col overflow-hidden ${isFullscreen ? '' : 'rounded-md'} ${
+        isMinimize ? 'invisible opacity-0' : ''
+      } `}
       onMouseDown={handleClickWindow}
       onTransitionEnd={handleTransitionEnd}
       ref={windowRef}
     >
       <div
-        className='flex h-7 items-center justify-center bg-[#e4e4e4]'
-        onMouseDown={handleDragElement}
-        onDoubleClick={handleMaximizeWindow}
+        className={`flex h-7 items-center justify-center bg-[#e5e7eb] ${
+          isFullscreen ? 'absolute inset-x-0 opacity-0 hover:static hover:opacity-100' : ''
+        }`}
+        onMouseDown={isFullscreen ? undefined : handleDragElement}
+        onDoubleClick={isFullscreen ? undefined : handleMaximizeWindow}
+        ref={titleBarRef}
       >
-        <ControlBox handleClose={handleClose} handleMinimize={handleMinimizeWindow} />
+        <ControlBox
+          handleClose={handleClose}
+          handleMinimize={handleMinimizeWindow}
+          handleFullscreen={handleFullscreen}
+          isFullscreen={isFullscreen}
+        />
         <span className='select-none font-bold'>{title}</span>
       </div>
       <div className='h-full overflow-auto'>{children}</div>
-      <div className='absolute left-2 right-2 top-0 h-1 cursor-row-resize' onMouseDown={handleResizeNorth} />
-      <div className='absolute bottom-0 left-2 right-2 h-1 cursor-row-resize' onMouseDown={handleResizeSouth} />
-      <div className='absolute bottom-2 left-0 top-2 w-1 cursor-col-resize' onMouseDown={handleResizeWest} />
-      <div className='absolute bottom-2 right-0 top-2 w-1 cursor-col-resize' onMouseDown={handleResizeEast} />
-      <div
-        className='absolute left-0 top-0 h-4 w-4 translate-x-[-50%] translate-y-[-50%] cursor-nw-resize'
-        onMouseDown={handleResizeNorthWest}
-      />
-      <div
-        className='absolute right-0 top-0 h-4 w-4 translate-x-[50%] translate-y-[-50%] cursor-ne-resize'
-        onMouseDown={handleResizeNorthEast}
-      />
-      <div
-        className='absolute bottom-0 left-0 h-4 w-4 translate-x-[-50%] translate-y-[50%] cursor-sw-resize'
-        onMouseDown={handleResizeSouthWest}
-      />
-      <div
-        className='absolute bottom-0 right-0 h-4 w-4 translate-x-[50%] translate-y-[50%] cursor-se-resize'
-        onMouseDown={handleResizeSouthEast}
-      />
+      {!isFullscreen && (
+        <>
+          <div className='absolute left-2 right-2 top-0 h-1 cursor-row-resize' onMouseDown={handleResizeNorth} />
+          <div className='absolute bottom-0 left-2 right-2 h-1 cursor-row-resize' onMouseDown={handleResizeSouth} />
+          <div className='absolute bottom-2 left-0 top-2 w-1 cursor-col-resize' onMouseDown={handleResizeWest} />
+          <div className='absolute bottom-2 right-0 top-2 w-1 cursor-col-resize' onMouseDown={handleResizeEast} />
+          <div
+            className='absolute left-0 top-0 h-4 w-4 translate-x-[-50%] translate-y-[-50%] cursor-nw-resize'
+            onMouseDown={handleResizeNorthWest}
+          />
+          <div
+            className='absolute right-0 top-0 h-4 w-4 translate-x-[50%] translate-y-[-50%] cursor-ne-resize'
+            onMouseDown={handleResizeNorthEast}
+          />
+          <div
+            className='absolute bottom-0 left-0 h-4 w-4 translate-x-[-50%] translate-y-[50%] cursor-sw-resize'
+            onMouseDown={handleResizeSouthWest}
+          />
+          <div
+            className='absolute bottom-0 right-0 h-4 w-4 translate-x-[50%] translate-y-[50%] cursor-se-resize'
+            onMouseDown={handleResizeSouthEast}
+          />
+        </>
+      )}
     </div>
   );
 };
