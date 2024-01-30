3c8dba2fd3d83a6c332bc02571410a5a2c644220
minjongbaek <minjongbaek@gmail.com>
2024-01-31 00:20:21 +0900

    feat: Spotlight 컴포넌트를 통해 앱을 띄울 수 있도록 구현

--- src/components/Spotlight/index.tsx
+++ src/components/Spotlight/index.tsx
@@ -1,12 +1,35 @@
 import SpotlightIcon from '@/assets/icons/SpotlightIcon.svg?react';
-import { useEffect, useRef } from 'react';
+import { AppReducerContext, AppStateContext } from '@/store/App/AppContext';
+import { useContext, useEffect, useRef, useState } from 'react';
 
 type SpotlightProps = {
   close: () => void;
 };
 
 const Spotlight = ({ close }: SpotlightProps) => {
+  const apps = useContext(AppStateContext);
+  const dispatch = useContext(AppReducerContext);
+
+  if (!dispatch) throw new Error('dispatch is null');
+
+  const [suggestedApps, setSuggestedApps] = useState<typeof apps>([]);
   const ref = useRef<HTMLDivElement>(null);
+
+  const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const keyword = event.target.value.toLowerCase();
+    if (keyword.trim() === '') {
+      setSuggestedApps([]);
+      return;
+    }
+    const selectedApps = apps.filter((app) => app.title.toLowerCase().includes(keyword));
+    setSuggestedApps(selectedApps);
+  };
+
+  const handleClickApp = (id: (typeof apps)[number]['id']) => {
+    dispatch({ type: 'OPEN', id });
+    close();
+    setSuggestedApps([]);
+  };
 
   useEffect(() => {
     const handleClickAway = (event: Event) => {
@@ -26,10 +49,23 @@ const Spotlight = ({ close }: SpotlightProps) => {
   }, [close]);
 
   return (
-    <div ref={ref} className='absolute left-1/2 top-1/4 z-50 -translate-x-1/2 -translate-y-1/2 transform'>
-      <div className='popup-container flex w-[36rem] items-center gap-2 rounded-2xl p-2.5'>
-        <SpotlightIcon width={24} height={24} stroke='none' />
-        <input placeholder='Spotlight Search' className='w-full bg-[rgba(255,_255,_255,_0)] text-[24px] outline-none' />
+    <div ref={ref} className='absolute left-1/2 top-[20%] z-50 -translate-x-1/2 transform'>
+      <div className='popup-container flex w-[36rem] flex-col rounded-2xl p-2.5'>
+        <div className='flex items-center gap-2'>
+          <SpotlightIcon width={24} height={24} stroke='none' />
+          <input
+            placeholder='Spotlight Search'
+            className='w-full bg-[rgba(255,_255,_255,_0)] text-[24px] outline-none'
+            onChange={handleChangeInput}
+          />
+        </div>
+        <ul className='space-y-2'>
+          {suggestedApps.map(({ id, title }) => (
+            <li key={id} className='select-none first-of-type:mt-2 hover:opacity-50' onClick={() => handleClickApp(id)}>
+              {title}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
