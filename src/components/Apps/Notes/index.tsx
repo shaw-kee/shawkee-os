aeeb8965eedccac3d7e9c951a3714a08137a8e1d
jaeungE <jaeung5169@naver.com>
2024-02-24 20:01:54 +0900

    feat: Note APP 기본 레이아웃 구현

--- src/components/Apps/Notes/index.tsx
+++ src/components/Apps/Notes/index.tsx
@@ -1,7 +1,16 @@
 const Notes = () => {
   return (
-    <div className='min-h-full bg-white'>
-      <div>Note App Content!</div>
+    <div className='flex min-h-full'>
+      <div className='flex w-full'>
+        <div className='flex-1 bg-[#dadada]/90 backdrop-blur-md'>SideBar</div>
+        <div className='flex flex-[5_5_0%] flex-col'>
+          <div className='h-12 border-b border-black/5 bg-[#EAEAEA]'>MenuBar</div>
+          <div className='flex grow'>
+            <div className='flex-[2_2_0%] border-r border-black/5 bg-white'>MemoList</div>
+            <div className='flex-[3_3_0%] bg-white'>NoteContent</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
