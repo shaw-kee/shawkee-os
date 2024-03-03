b4abae03495463c3ee7f8ac4197a520076fe3211
minjongbaek <minjongbaek@gmail.com>
2024-03-03 17:47:47 +0900

    feat:  AppWindow 컴포넌트에서 mouse up 이벤트를 통해 사이즈 상태가 업데이트 되도록 구현

--- src/components/AppWindow/useRND.ts
+++ src/components/AppWindow/useRND.ts
@@ -11,107 +11,157 @@ const useRND = (initialPosition: Position, minSize: Size, boundary: Size) => {
   const { width: boundaryWidth, height: boundaryHeight } = boundary;
   const [{ width, height }, setSize] = useState<Size>({ width: minWidth, height: minHeight });
   const [{ x, y }, setPosition] = useState<Position>({ x: initialX, y: initialY });
-  const tempPosition = { x: initialX, y: initialY };
+  const positionRef = useRef({ x: initialX, y: initialY });
+  const sizeRef = useRef({ width: minWidth, height: minHeight });
+
   const handleMouseUp = () => {
-    setPosition(tempPosition);
+    setResize({ ...positionRef.current, ...sizeRef.current });
   };
 
   const { handleMouseDown: handleDragElement } = mouseDrag((moveX, moveY) => {
     if (!ref.current) return;
+
     const calculatedX = clampValue(x + moveX, BOUNDARY_MIN - width + BOUNDARY_MARGIN, boundary.width - BOUNDARY_MARGIN);
     const calculatedY = clampValue(y + moveY, BOUNDARY_MIN, boundary.height - BOUNDARY_MARGIN);
-    tempPosition.x = calculatedX;
-    tempPosition.y = calculatedY;
+
+    positionRef.current = { x: calculatedX, y: calculatedY };
+
     ref.current.style.transform = `translate(${calculatedX}px,${calculatedY}px)`;
   }, handleMouseUp);
 
   const { handleMouseDown: handleResizeNorthWest } = mouseDrag((moveX, moveY) => {
-    setSize({
-      width: clampValue(width - moveX, minWidth, x + width),
-      height: clampValue(height - moveY, minHeight, y + height),
-    });
-    setPosition({
-      x: clampValue(x + moveX, BOUNDARY_MIN, x + width - minWidth),
-      y: clampValue(y + moveY, BOUNDARY_MIN, y + height - minHeight),
-    });
-  });
+    if (!ref.current) return;
+    const calculatedWidth = clampValue(width - moveX, minWidth, x + width);
+    const calculatedHeight = clampValue(height - moveY, minHeight, y + height);
+
+    const calculatedX = clampValue(x + moveX, BOUNDARY_MIN, x + width - minWidth);
+    const calculatedY = clampValue(y + moveY, BOUNDARY_MIN, y + height - minHeight);
+
+    sizeRef.current = { width: calculatedWidth, height: calculatedHeight };
+    positionRef.current = { x: calculatedX, y: calculatedY };
+
+    ref.current.style.transform = `translate(${calculatedX}px,${calculatedY}px)`;
+    ref.current.style.width = `${calculatedWidth}px`;
+    ref.current.style.height = `${calculatedHeight}px`;
+  }, handleMouseUp);
 
   const { handleMouseDown: handleResizeNorthEast } = mouseDrag((moveX, moveY) => {
-    setSize({
-      width: clampValue(width + moveX, minWidth, boundaryWidth - x),
-      height: clampValue(height - moveY, minHeight, y + height),
-    });
-    setPosition({
-      x,
-      y: clampValue(y + moveY, BOUNDARY_MIN, y + height - minHeight),
-    });
-  });
+    if (!ref.current) return;
+
+    const calculatedWidth = clampValue(width + moveX, minWidth, boundaryWidth - x);
+    const calculatedHeight = clampValue(height - moveY, minHeight, y + height);
+
+    const calculatedX = x;
+    const calculatedY = clampValue(y + moveY, BOUNDARY_MIN, y + height - minHeight);
+
+    sizeRef.current = { width: calculatedWidth, height: calculatedHeight };
+    positionRef.current = { x: calculatedX, y: calculatedY };
+
+    ref.current.style.transform = `translate(${calculatedX}px,${calculatedY}px)`;
+    ref.current.style.width = `${calculatedWidth}px`;
+    ref.current.style.height = `${calculatedHeight}px`;
+  }, handleMouseUp);
 
   const { handleMouseDown: handleResizeSouthWest } = mouseDrag((moveX, moveY) => {
-    setSize({
-      width: clampValue(width - moveX, minWidth, x + width),
-      height: clampValue(height + moveY, minHeight, boundaryHeight - y),
-    });
-    setPosition({
-      x: clampValue(x + moveX, BOUNDARY_MIN, x + width - minWidth),
-      y,
-    });
-  });
+    if (!ref.current) return;
+
+    const calculatedWidth = clampValue(width - moveX, minWidth, x + width);
+    const calculatedHeight = clampValue(height + moveY, minHeight, boundaryHeight - y);
+
+    const calculatedX = clampValue(x + moveX, BOUNDARY_MIN, x + width - minWidth);
+    const calculatedY = y;
+
+    sizeRef.current = { width: calculatedWidth, height: calculatedHeight };
+    positionRef.current = { x: calculatedX, y: calculatedY };
+
+    ref.current.style.transform = `translate(${calculatedX}px,${calculatedY}px)`;
+    ref.current.style.width = `${calculatedWidth}px`;
+    ref.current.style.height = `${calculatedHeight}px`;
+  }, handleMouseUp);
 
   const { handleMouseDown: handleResizeSouthEast } = mouseDrag((moveX, moveY) => {
-    setSize({
-      width: clampValue(width + moveX, minWidth, boundaryWidth - x),
-      height: clampValue(height + moveY, minHeight, boundaryHeight - y),
-    });
-    setPosition({
-      x,
-      y,
-    });
-  });
+    if (!ref.current) return;
+
+    const calculatedWidth = clampValue(width + moveX, minWidth, boundaryWidth - x);
+    const calculatedHeight = clampValue(height + moveY, minHeight, boundaryHeight - y);
+
+    const calculatedX = x;
+    const calculatedY = y;
+
+    sizeRef.current = { width: calculatedWidth, height: calculatedHeight };
+    positionRef.current = { x: calculatedX, y: calculatedY };
+
+    ref.current.style.transform = `translate(${calculatedX}px,${calculatedY}px)`;
+    ref.current.style.width = `${calculatedWidth}px`;
+    ref.current.style.height = `${calculatedHeight}px`;
+  }, handleMouseUp);
 
   const { handleMouseDown: handleResizeWest } = mouseDrag((moveX) => {
-    setSize({
-      width: clampValue(width - moveX, minWidth, x + width),
-      height,
-    });
-    setPosition({
-      x: clampValue(x + moveX, BOUNDARY_MIN, x + width - minWidth),
-      y,
-    });
-  });
+    if (!ref.current) return;
+
+    const calculatedWidth = clampValue(width - moveX, minWidth, x + width);
+    const calculatedHeight = height;
+
+    const calculatedX = clampValue(x + moveX, BOUNDARY_MIN, x + width - minWidth);
+    const calculatedY = y;
+
+    sizeRef.current = { width: calculatedWidth, height: calculatedHeight };
+    positionRef.current = { x: calculatedX, y: calculatedY };
+
+    ref.current.style.transform = `translate(${calculatedX}px,${calculatedY}px)`;
+    ref.current.style.width = `${calculatedWidth}px`;
+    ref.current.style.height = `${calculatedHeight}px`;
+  }, handleMouseUp);
 
   const { handleMouseDown: handleResizeEast } = mouseDrag((moveX) => {
-    setSize({
-      width: clampValue(width + moveX, minWidth, boundaryWidth - x),
-      height,
-    });
-    setPosition({
-      x,
-      y,
-    });
-  });
+    if (!ref.current) return;
+
+    const calculatedWidth = clampValue(width + moveX, minWidth, boundaryWidth - x);
+    const calculatedHeight = height;
+
+    const calculatedX = x;
+    const calculatedY = y;
+
+    sizeRef.current = { width: calculatedWidth, height: calculatedHeight };
+    positionRef.current = { x: calculatedX, y: calculatedY };
+
+    ref.current.style.transform = `translate(${calculatedX}px,${calculatedY}px)`;
+    ref.current.style.width = `${calculatedWidth}px`;
+    ref.current.style.height = `${calculatedHeight}px`;
+  }, handleMouseUp);
 
   const { handleMouseDown: handleResizeNorth } = mouseDrag((_, moveY) => {
-    setSize({
-      width,
-      height: clampValue(height - moveY, minHeight, y + height),
-    });
-    setPosition({
-      x,
-      y: clampValue(y + moveY, BOUNDARY_MIN, y + height - minHeight),
-    });
-  });
+    if (!ref.current) return;
+    const calculatedWidth = width;
+    const calculatedHeight = clampValue(height - moveY, minHeight, y + height);
+
+    const calculatedX = x;
+    const calculatedY = clampValue(y + moveY, BOUNDARY_MIN, y + height - minHeight);
+
+    sizeRef.current = { width: calculatedWidth, height: calculatedHeight };
+    positionRef.current = { x: calculatedX, y: calculatedY };
+
+    ref.current.style.transform = `translate(${calculatedX}px,${calculatedY}px)`;
+    ref.current.style.width = `${calculatedWidth}px`;
+    ref.current.style.height = `${calculatedHeight}px`;
+  }, handleMouseUp);
 
   const { handleMouseDown: handleResizeSouth } = mouseDrag((_, moveY) => {
-    setSize({
-      width,
-      height: clampValue(height + moveY, minHeight, boundaryHeight - y),
-    });
-    setPosition({
-      x,
-      y,
-    });
-  });
+    if (!ref.current) return;
+
+    const calculatedWidth = width;
+    const calculatedHeight = clampValue(height + moveY, minHeight, boundaryHeight - y);
+
+    const calculatedX = x;
+    const calculatedY = y;
+
+    sizeRef.current = { width: calculatedWidth, height: calculatedHeight };
+    positionRef.current = { x: calculatedX, y: calculatedY };
+
+    ref.current.style.transform = `translate(${calculatedX}px,${calculatedY}px)`;
+    ref.current.style.width = `${calculatedWidth}px`;
+    ref.current.style.height = `${calculatedHeight}px`;
+  }, handleMouseUp);
 
   const setResize = ({ x, y, width, height }: Position & Size) => {
     setPosition({ x, y });
