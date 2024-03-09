77b00add5c79d0b5f5b2a10163b547f1e656358a
minjongbaek <minjongbaek@gmail.com>
2024-03-09 15:08:54 +0900

    feat: Safari 컴포넌트 검색창에 공백이 입력되지 않도록 수정

--- src/components/Apps/Safari/index.tsx
+++ src/components/Apps/Safari/index.tsx
@@ -1,11 +1,12 @@
 import { type KeyboardEvent, type FocusEvent, useState, useEffect, useRef } from 'react';
+import HomePage from './HomePage';
 
 const Safari = () => {
   const [iframeSrc, setIframeSrc] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
   const iframeRef = useRef<HTMLIFrameElement>(null);
 
   const handleFocusInput = (event: FocusEvent<HTMLInputElement, Element>) => {
-    console.log('click!');
     event.currentTarget.setSelectionRange(0, event.currentTarget.value.length);
   };
 
@@ -13,11 +14,13 @@ const Safari = () => {
     if (event.key === 'Enter') {
       let value = event.currentTarget.value;
 
+      if (!value.trim()) return;
+
       if (!checkUrlProtocol(value)) {
         value = 'https://' + value;
-      }
 
-      event.currentTarget.value = new URL(value).hostname;
+        event.currentTarget.value = new URL(value).hostname;
+      }
 
       setIframeSrc(value);
       event.currentTarget.blur();
@@ -46,20 +49,54 @@ const Safari = () => {
     return () => observer.disconnect();
   }, [iframeSrc]);
 
+  const handleClickBackButton = () => {
+    alert('back');
+  };
+
+  const handleClickNextButton = () => {
+    alert('next');
+  };
+
+  const handleClickRefreshButton = () => {
+    alert('refresh');
+  };
+
+  const handleClickHomeButton = () => {
+    if (inputRef.current) inputRef.current.value = '';
+    setTimeout(() => setIframeSrc(''), 100);
+  };
+
   return (
     <div className='flex min-h-full w-full flex-col bg-white'>
-      <div className='flex items-center gap-1 bg-gray-100 px-2 py-2'>
-        <button className='aspect-square w-10 rounded-full transition-colors duration-300 hover:bg-gray-200'>{`<`}</button>
-        <button className='aspect-square w-10 rounded-full transition-colors duration-300 hover:bg-gray-200'>{`>`}</button>
-        <button className='aspect-square w-10 rounded-full transition-colors duration-300 hover:bg-gray-200'>R</button>
-        <button className='aspect-square w-10 rounded-full transition-colors duration-300 hover:bg-gray-200'>H</button>
+      <div className='flex select-none items-center gap-1 bg-gray-100 px-2 py-2'>
+        <button
+          className='aspect-square w-10 rounded-full transition-colors duration-300 hover:bg-gray-200'
+          onClick={handleClickBackButton}
+        >{`<`}</button>
+        <button
+          className='aspect-square w-10 rounded-full transition-colors duration-300 hover:bg-gray-200'
+          onClick={handleClickNextButton}
+        >{`>`}</button>
+        <button
+          className='aspect-square w-10 rounded-full transition-colors duration-300 hover:bg-gray-200'
+          onClick={handleClickRefreshButton}
+        >
+          R
+        </button>
+        <button
+          className='aspect-square w-10 rounded-full transition-colors duration-300 hover:bg-gray-200'
+          onClick={handleClickHomeButton}
+        >
+          H
+        </button>
         <input
+          ref={inputRef}
           className='w-full rounded-full bg-gray-200 px-3 py-1 focus:bg-white'
           onKeyDown={handleKeydown}
           onFocus={handleFocusInput}
         />
       </div>
-      {iframeSrc ? <iframe ref={iframeRef} className='grow' src={iframeSrc} /> : <div className='grow bg-blue-400' />}
+      {iframeSrc ? <iframe ref={iframeRef} className='grow' src={iframeSrc} /> : <HomePage />}
     </div>
   );
 };
