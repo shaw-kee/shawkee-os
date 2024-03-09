0286050bc96ef3dbee703a400d2d9db4bf2d7baf
jaeungE <jaeung5169@naver.com>
2024-03-09 23:10:04 +0900

    feat: NoteContent UI 구현

--- src/components/Apps/Notes/NoteContent.tsx
+++ src/components/Apps/Notes/NoteContent.tsx
@@ -1,5 +1,13 @@
 const NoteContent = () => {
-  return <div className='flex-[3_3_0%] bg-white'>NoteContent</div>;
+  return (
+    <div className='flex flex-[3_3_0%] flex-col justify-center bg-white'>
+      <span className='mt-2 select-none text-center text-sm font-bold text-black/30'>2024년 2월 24일 오후 10:42</span>
+      <div className='mt-2 flex grow flex-col px-4'>
+        <input className='text-lg font-bold outline-none' />
+        <textarea className='mt-2 grow resize-none text-sm focus:outline-none' />
+      </div>
+    </div>
+  );
 };
 
 export default NoteContent;
