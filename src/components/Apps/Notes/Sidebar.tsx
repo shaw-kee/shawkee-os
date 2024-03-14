4fe737558a678de32cff8f9bf546e22a5f7c0674
jaeungE <jaeung5169@naver.com>
2024-03-15 03:28:26 +0900

    design: Sidebar item 배경 색상 변경

--- src/components/Apps/Notes/Sidebar.tsx
+++ src/components/Apps/Notes/Sidebar.tsx
@@ -3,7 +3,7 @@ import DirectoryIcon from '@/assets/icons/Notes/Directory.svg?react';
 
 const Sidebar = () => {
   return (
-    <div className='flex w-40 flex-col bg-[#e6e2e6]/95 backdrop-blur-md'>
+    <div className='flex w-40 select-none flex-col bg-[#e6e2e6]/95 backdrop-blur-md'>
       <div className='relative h-12'>
         <div className='absolute right-4 top-[50%] flex translate-y-[-50%] items-center'>
           <button className='rounded-lg hover:bg-[#d9d7da]'>
@@ -17,7 +17,7 @@ const Sidebar = () => {
         <span className='pl-1 text-[11px] font-bold text-black/25'>iCloud</span>
         <div className='flex flex-row'>
           <ul className='w-full'>
-            <li className='flex items-center justify-between rounded-md p-1'>
+            <li className='flex items-center justify-between rounded-md bg-[#c7c4c7] p-1'>
               <div className='flex gap-1'>
                 <div className='h-5 w-5'>
                   <DirectoryIcon width='20' height='20' viewBox='0 0 28 28' color='#007AFF' />
