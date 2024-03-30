bae67b50a9a25a0d6cfcad370d6e61197324cd93
minjongbaek <minjongbaek@gmail.com>
2024-03-30 13:12:03 +0900

    feat: `navigator.mediaDevices` 를 이용해 미디어 스트림을 video 객체에 전달하도록 구현

--- src/components/Apps/PhotoBooth/index.tsx
+++ src/components/Apps/PhotoBooth/index.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useRef } from 'react';
+
+const mediaStreamConstraints = {
+  audio: false,
+  video: { width: 800, height: 600 },
+};
+
 const PhotoBooth = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia(mediaStreamConstraints)
+      .then((mediaStream) => {
+        if (!videoRef.current) return;
+        videoRef.current.srcObject = mediaStream;
+        videoRef.current.onloadedmetadata = () => {
+          videoRef.current?.play();
+        };
+      })
+      .catch(() => console.error('Can not found video'));
+  }, []);
+
   return (
     <div className='min-h-full bg-white'>
-      <div>PhotoBooth App Content!</div>
+      <video ref={videoRef} className='[transform:rotateY(180deg)]' />
     </div>
   );
 };
