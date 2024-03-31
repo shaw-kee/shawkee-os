abe6dc4759fe2c58bb083e490429ea58bd15a487
minjongbaek <minjongbaek@gmail.com>
2024-03-31 14:50:16 +0900

    feat: 버튼을 눌러 현재 화상을 캡쳐하도록 구현

--- src/components/Apps/PhotoBooth/index.tsx
+++ src/components/Apps/PhotoBooth/index.tsx
@@ -1,29 +1,38 @@
-import { useEffect, useRef } from 'react';
-
-const mediaStreamConstraints = {
-  audio: false,
-  video: { width: 800, height: 600 },
-};
+import { useRef, useState } from 'react';
+import Webcam from 'react-webcam';
+import CameraIcon from '@/assets/icons/PhotoBooth/CameraIcon.svg?react';
 
 const PhotoBooth = () => {
-  const videoRef = useRef<HTMLVideoElement>(null);
+  const [camLoaded, setCamLoaded] = useState(false);
+  const webcamRef = useRef<Webcam>(null);
 
-  useEffect(() => {
-    navigator.mediaDevices
-      .getUserMedia(mediaStreamConstraints)
-      .then((mediaStream) => {
-        if (!videoRef.current) return;
-        videoRef.current.srcObject = mediaStream;
-        videoRef.current.onloadedmetadata = () => {
-          videoRef.current?.play();
-        };
-      })
-      .catch(() => console.error('Can not found video'));
-  }, []);
+  const webcamClassNames = camLoaded ? 'opacity-100' : 'opacity-0';
+
+  const handleClickButton = () => {
+    const screenshot = webcamRef.current?.getScreenshot();
+    console.log(screenshot);
+  };
 
   return (
-    <div className='min-h-full bg-white'>
-      <video ref={videoRef} className='[transform:rotateY(180deg)]' />
+    <div className='flex min-h-full flex-col bg-white'>
+      <div className='relative flex-grow bg-black'>
+        <Webcam
+          className={`${webcamClassNames} absolute h-full w-full flex-grow object-cover transition-opacity duration-500`}
+          mirrored={true}
+          audio={false}
+          screenshotFormat='image/png'
+          videoConstraints={{
+            facingMode: 'user',
+          }}
+          onUserMedia={() => setCamLoaded(true)}
+          ref={webcamRef}
+        />
+      </div>
+      <div className='flex w-full items-center justify-center bg-[#e5e7eb] py-1'>
+        <button className='cursor-auto rounded-full bg-red-500 p-2 active:bg-red-600' onClick={handleClickButton}>
+          <CameraIcon width={36} height={36} fill='#fff' />
+        </button>
+      </div>
     </div>
   );
 };
