98b1220c212b0a7a76e98b9fd4211006263a34de
jaeungE <jaeung5169@naver.com>
2024-04-03 02:12:41 +0900

    feat: Calculator 기본 ui 구현

--- src/components/Apps/Calculator/index.tsx
+++ src/components/Apps/Calculator/index.tsx
@@ -1,7 +1,25 @@
 const Calculator = () => {
   return (
-    <div className='min-h-full bg-white'>
-      <div>Calculator App Content!</div>
+    <div className='flex h-full flex-col bg-[#3d3d3d]/80'>
+      <div className='flex justify-end px-6 text-[4rem] font-thin text-white'>0</div>
+      <div className='grid grow grid-cols-4 gap-[2px]'>
+        <button className='bg-[#676569]/50 text-[1.5rem] font-normal text-white'>AC</button>
+        <button className='bg-[#676569]/50 text-[1.5rem] font-normal text-white'></button>
+        <button className='bg-[#676569]/50 text-[1.5rem] font-normal text-white'>%</button>
+        <button className='bg-[#ff970e] text-[1.5rem] font-normal text-white'>÷</button>
+        <button className='bg-[#807e82]/80 text-[1.5rem] font-normal text-white'>7</button>
+        <button className='bg-[#807e82]/80 text-[1.5rem] font-normal text-white'>8</button>
+        <button className='bg-[#807e82]/80 text-[1.5rem] font-normal text-white'>9</button>
+        <button className='bg-[#ff970e] text-[1.5rem] font-normal text-white'>x</button>
+        <button className='bg-[#807e82]/80 text-[1.5rem] font-normal text-white'>4</button>
+        <button className='bg-[#807e82]/80 text-[1.5rem] font-normal text-white'>5</button>
+        <button className='bg-[#807e82]/80 text-[1.5rem] font-normal text-white'>6</button>
+        <button className='bg-[#ff970e] text-[1.5rem] font-normal text-white'>-</button>
+        <button className='bg-[#807e82]/80 text-[1.5rem] font-normal text-white'>1</button>
+        <button className='bg-[#807e82]/80 text-[1.5rem] font-normal text-white'>2</button>
+        <button className='bg-[#807e82]/80 text-[1.5rem] font-normal text-white'>3</button>
+        <button className='bg-[#ff970e] text-[1.5rem] font-normal text-white'>+</button>
+      </div>
     </div>
   );
 };
