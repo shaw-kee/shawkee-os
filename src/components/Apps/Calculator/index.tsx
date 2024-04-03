045c104e5c891c7dc47e767bddde520f3afcdcd5
jaeungE <jaeung5169@naver.com>
2024-04-03 17:49:15 +0900

    design: calculator inset shadow 색상 변경

--- src/components/Apps/Calculator/index.tsx
+++ src/components/Apps/Calculator/index.tsx
@@ -1,6 +1,6 @@
 const Calculator = () => {
   return (
-    <div className='flex h-full flex-col bg-[#3d3d3d]/80 shadow-[inset_0_0_2px_2px_rgba(135,135,135,0.8)]'>
+    <div className='flex h-full flex-col bg-[#3d3d3d]/80 shadow-[inset_0_0_2px_1px_rgba(187,187,187,0.8)]'>
       <div className='flex justify-end px-6 text-[4rem] font-thin text-white'>0</div>
       <div className='grid grow grid-cols-4 gap-[2px]'>
         <button className='bg-[#676569]/50 text-[1.5rem] font-normal text-white'>AC</button>
