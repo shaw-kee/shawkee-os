83e829c7e4a05b251029cadccef5af78041dfbba
jaeungE <jaeung5169@naver.com>
2024-04-03 23:15:53 +0900

    design: button active 스타일 추가

--- src/components/Apps/Calculator/index.tsx
+++ src/components/Apps/Calculator/index.tsx
@@ -11,37 +11,39 @@ const Calculator = () => {
     <div className='flex h-full flex-col bg-[#595559]/95 shadow-[inset_0_0_2px_1px_rgba(187,187,187,0.8)]'>
       <div className='flex justify-end px-6 text-[4rem] font-thin text-white'>0</div>
       <div className='grid grow grid-cols-4 gap-[2px]'>
-        <button className='bg-calc-function text-[1.5rem] font-normal text-white'>AC</button>
-        <button className='bg-calc-function flex items-center justify-center text-[1.5rem] font-normal text-white'>
+        <button className='bg-calc-function active:bg-calc-number text-[1.5rem] font-normal text-white'>AC</button>
+        <button className='bg-calc-function active:bg-calc-number flex items-center justify-center text-[1.5rem] font-normal text-white'>
           <PlusMinusIcon width='28' height='28' fill='white' />
         </button>
-        <button className='bg-calc-function flex items-center justify-center text-[1.5rem] font-normal text-white'>
+        <button className='bg-calc-function active:bg-calc-number flex items-center justify-center text-[1.5rem] font-normal text-white'>
           <PercentIcon width='28' height='28' fill='white' />
         </button>
-        <button className='bg-calc-operator flex items-center justify-center text-[1.5rem] font-normal text-white'>
+        <button className='bg-calc-operator flex items-center justify-center text-[1.5rem] font-normal text-white active:bg-[#ff970e]/70'>
           <DivideIcon width='28' height='28' fill='white' />
         </button>
-        <button className='bg-calc-number text-[1.5rem] font-normal text-white'>7</button>
-        <button className='bg-calc-number text-[1.5rem] font-normal text-white'>8</button>
-        <button className='bg-calc-number text-[1.5rem] font-normal text-white'>9</button>
-        <button className='bg-calc-operator flex items-center justify-center text-[1.5rem] font-normal text-white'>
+        <button className='bg-calc-number text-[1.5rem] font-normal text-white active:bg-[#aba8ad]'>7</button>
+        <button className='bg-calc-number text-[1.5rem] font-normal text-white active:bg-[#aba8ad]'>8</button>
+        <button className='bg-calc-number text-[1.5rem] font-normal text-white active:bg-[#aba8ad]'>9</button>
+        <button className='bg-calc-operator flex items-center justify-center text-[1.5rem] font-normal text-white active:bg-[#ff970e]/70'>
           <MultiplyIcon width='28' height='28' fill='white' />
         </button>
-        <button className='bg-calc-number text-[1.5rem] font-normal text-white'>4</button>
-        <button className='bg-calc-number text-[1.5rem] font-normal text-white'>5</button>
-        <button className='bg-calc-number text-[1.5rem] font-normal text-white'>6</button>
-        <button className='bg-calc-operator flex items-center justify-center text-[1.5rem] font-normal text-white'>
+        <button className='bg-calc-number text-[1.5rem] font-normal text-white active:bg-[#aba8ad]'>4</button>
+        <button className='bg-calc-number text-[1.5rem] font-normal text-white active:bg-[#aba8ad]'>5</button>
+        <button className='bg-calc-number text-[1.5rem] font-normal text-white active:bg-[#aba8ad]'>6</button>
+        <button className='bg-calc-operator flex items-center justify-center text-[1.5rem] font-normal text-white active:bg-[#ff970e]/70'>
           <MinusIcon width='28' height='28' fill='white' />
         </button>
-        <button className='bg-calc-number text-[1.5rem] font-normal text-white'>1</button>
-        <button className='bg-calc-number text-[1.5rem] font-normal text-white'>2</button>
-        <button className='bg-calc-number text-[1.5rem] font-normal text-white'>3</button>
-        <button className='bg-calc-operator flex items-center justify-center text-[1.5rem] font-normal text-white'>
+        <button className='bg-calc-number text-[1.5rem] font-normal text-white active:bg-[#aba8ad]'>1</button>
+        <button className='bg-calc-number text-[1.5rem] font-normal text-white active:bg-[#aba8ad]'>2</button>
+        <button className='bg-calc-number text-[1.5rem] font-normal text-white active:bg-[#aba8ad]'>3</button>
+        <button className='bg-calc-operator flex items-center justify-center text-[1.5rem] font-normal text-white active:bg-[#ff970e]/70'>
           <PlusIcon width='28' height='28' fill='white' />
         </button>
-        <button className='bg-calc-number col-span-2 text-[1.5rem] font-normal text-white'>0</button>
-        <button className='bg-calc-number text-[1.5rem] font-normal text-white'>.</button>
-        <button className='bg-calc-operator flex items-center justify-center text-[1.5rem] font-normal text-white'>
+        <button className='bg-calc-number col-span-2 text-[1.5rem] font-normal text-white active:bg-[#aba8ad]'>
+          0
+        </button>
+        <button className='bg-calc-number text-[1.5rem] font-normal text-white active:bg-[#aba8ad]'>.</button>
+        <button className='bg-calc-operator flex items-center justify-center text-[1.5rem] font-normal text-white active:bg-[#ff970e]/70'>
           <EqualIcon width='28' height='28' fill='white' />
         </button>
       </div>
