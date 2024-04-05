2bad3135040d4f131b72507a339cc19e33762bfc
jaeungE <jaeung5169@naver.com>
2024-04-06 04:27:57 +0900

    feat: 사칙연산 구현

--- src/components/Apps/Calculator/index.tsx
+++ src/components/Apps/Calculator/index.tsx
@@ -1,9 +1,69 @@
 import { functionKeys, icons, numbers, operators } from '@/constants/calculator';
+import { useState } from 'react';
+const defaultState = { operand: '', formulaOperator: '' };
 
 const Calculator = () => {
+  const [formula, setFormula] = useState(defaultState);
+  const [input, setInput] = useState('0');
+
+  const handleClickNumber = (number: string) => {
+    if (formula.formulaOperator === '=') {
+      setFormula({ ...formula, formulaOperator: '' });
+    }
+
+    setInput((prev) => {
+      if (prev !== '0' || number === '.') {
+        return prev + number;
+      }
+
+      return number;
+    });
+  };
+
+  const handleClickOperator = (operator: string) => {
+    const { operand, formulaOperator } = formula;
+    if (input === '0' && operand === '') return;
+    if (formulaOperator === '=') {
+      setFormula({ ...formula, formulaOperator: operator });
+      return;
+    }
+    setInput('0');
+
+    if (formulaOperator === '') {
+      setFormula({ formulaOperator: operator, operand: input });
+    }
+
+    if (formulaOperator !== '') {
+      setFormula({ formulaOperator: operator, operand: calculate(operand, formulaOperator, input) });
+    }
+
+    if (operator === '=') {
+      setInput('0');
+    }
+  };
+
+  const calculate = (operandA: string, operator: string, operandB: string) => {
+    switch (operator) {
+      case '+':
+        return String(Number(operandA) + Number(operandB));
+      case '-':
+        return String(Number(operandA) - Number(operandB));
+      case '*':
+        return String(Number(operandA) * Number(operandB));
+      case '/':
+        return String(Number(operandA) / Number(operandB));
+    }
+    return '';
+  };
+
+  const displayResult = () => {
+    if (formula.operand !== '' && input === '0') return formula.operand;
+    return input;
+  };
+
   return (
     <div className='flex h-full flex-col bg-[#595559]/95 shadow-[inset_0_0_2px_1px_rgba(187,187,187,0.8)]'>
-      <div className='flex justify-end px-6 text-[4rem] font-thin text-white'>0</div>
+      <div className='flex justify-end px-6 text-[4rem] font-thin text-white'>{displayResult()}</div>
       <div className='grid grow grid-cols-4 grid-rows-5 gap-[2px]'>
         <div className='col-span-3 row-span-1 flex gap-[2px]'>
           {functionKeys.map((functionKey) => (
@@ -17,10 +77,11 @@ const Calculator = () => {
         </div>
         <div className='col-span-3 row-span-4 grid grid-cols-3 grid-rows-4 gap-[2px]'>
           {numbers.map((number) =>
-            number === 0 ? (
+            number === '0' ? (
               <button
                 className='col-span-2 bg-calc-number text-[1.5rem] font-normal text-white active:bg-[#aba8ad]'
                 key={`calc-${number}`}
+                onClick={() => handleClickNumber(number)}
               >
                 {number}
               </button>
@@ -28,6 +89,7 @@ const Calculator = () => {
               <button
                 className='bg-calc-number text-[1.5rem] font-normal text-white active:bg-[#aba8ad]'
                 key={`calc-${number}`}
+                onClick={() => handleClickNumber(number)}
               >
                 {number}
               </button>
@@ -39,6 +101,7 @@ const Calculator = () => {
             <button
               className='flex flex-1 items-center justify-center bg-calc-operator text-[1.5rem] font-normal text-white active:bg-[#ff970e]/70'
               key={`calc-${operator}`}
+              onClick={() => handleClickOperator(operator)}
             >
               {icons[operator]}
             </button>
