8e385d573b855ddf51da0f7a2758b47fc7e6718e
jaeungE <jaeung5169@naver.com>
2024-04-11 22:25:56 +0900

    feat: Terminal 기본 기능 구현

--- src/components/Apps/Terminal/index.tsx
+++ src/components/Apps/Terminal/index.tsx
@@ -1,17 +1,65 @@
+import React, { ReactElement, useCallback, useRef, useState } from 'react';
+
+interface ContentType {
+  id: string;
+  content: ReactElement;
+}
+
 const Terminal = () => {
-  return (
-    <div className='flex h-full flex-col bg-slate-800/95 p-3'>
-      <div className='text-white'>
-        <span>Hello World! Type `help` to get started</span>
-      </div>
+  const [contents, setContents] = useState<ContentType[]>([]);
+  const currentInputRef = useRef<HTMLInputElement>();
+
+  const addContent = (content: ReactElement) => {
+    setContents((prevContents) => [...prevContents, { id: Date.now().toString(), content }]);
+  };
+
+  const generateInput = () => {
+    return (
       <div className='flex gap-1'>
         <div className='flex gap-1'>
           <span className='text-green-400'>shawkee@macbook-pro</span>
           <span className='text-yellow-500'>~</span>
         </div>
         <span className='font-bold text-slate-400'>&gt;</span>
-        <input className='bg-transparent text-white caret-slate-400 outline-none'></input>
+        <input
+          className='grow bg-transparent text-white caret-slate-400 outline-none'
+          ref={callbackInputRef}
+          autoFocus
+        />
       </div>
+    );
+  };
+
+  const handleKeydown = useCallback((e: KeyboardEvent) => {
+    if (!currentInputRef.current) return;
+
+    if (e.key === 'Enter') {
+      currentInputRef.current.readOnly = true;
+      addContent(generateInput());
+    }
+  }, []);
+
+  const callbackInputRef = (element: HTMLInputElement) => {
+    if (element) {
+      element.addEventListener('keydown', handleKeydown);
+      currentInputRef.current = element;
+    }
+  };
+
+  const handleClick = () => {
+    if (!currentInputRef.current) return;
+    currentInputRef.current.focus();
+  };
+
+  return (
+    <div className='flex h-full flex-col overflow-x-auto bg-slate-800/95 p-3' onClick={handleClick}>
+      <div className='text-white'>
+        <span>Hello World! Type `help` to get started</span>
+      </div>
+      {generateInput()}
+      {contents.map(({ id, content }) => (
+        <React.Fragment key={id}>{content}</React.Fragment>
+      ))}
     </div>
   );
 };
