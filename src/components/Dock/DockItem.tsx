8a2223dd15e3e1698553c08db064b3feb056933d
jaeungE <jaeung5169@naver.com>
2024-04-18 23:27:35 +0900

    feat: DockItem html 구조 변경

--- src/components/Dock/DockItem.tsx
+++ src/components/Dock/DockItem.tsx
@@ -31,13 +31,15 @@ const DockItem = ({ id, title, imageUrl, isOpen = false, link = '', mousePositio
       className='tooltip flex flex-col items-center justify-end hover:cursor-pointer'
       onClick={handleClick}
     >
-      <ConditionalLink link={link}>
-        <div className='tooltip-text top-[calc(-100%-10px)] inline-flex items-center rounded-md bg-[#F6F6F6B8] px-3 py-1 text-sm text-black shadow-[0_2px_6px_0_rgba(0,0,0,0.20),0_0_0_0.5px_rgba(0,0,0,0.12)]'>
-          {title}
-        </div>
-        <img src={imageUrl} className='aspect-square w-full active:contrast-50' draggable={false} />
-        <div className={`absolute bottom-[2px] h-1 w-1 rounded-full bg-[#575757] ${isOpen ? '' : 'hidden'}`} />
-      </ConditionalLink>
+      <div className='tooltip-text top-[calc(-100%-20px)] inline-flex items-center rounded-md bg-[#F6F6F6B8] px-3 py-1 text-sm text-black shadow-[0_2px_6px_0_rgba(0,0,0,0.20),0_0_0_0.5px_rgba(0,0,0,0.12)]'>
+        {title}
+      </div>
+      <div>
+        <ConditionalLink link={link}>
+          <img src={imageUrl} className='aspect-square w-full active:contrast-50' draggable={false} />
+          <div className={`absolute bottom-[2px] h-1 w-1 rounded-full bg-[#575757] ${isOpen ? '' : 'hidden'}`} />
+        </ConditionalLink>
+      </div>
     </motion.div>
   );
 };
