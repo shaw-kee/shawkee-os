f19d0630e9f5df89dffeefc99719fd473f967d92
minjongbaek <minjongbaek@gmail.com>
2024-05-02 22:52:21 +0900

    refactor: SnakeGame useEffect 내부에서 처리한 canvas 제어 로직을 SnakeGameManager 클래스로 이동

--- src/components/Apps/SnakeGame/index.tsx
+++ src/components/Apps/SnakeGame/index.tsx
@@ -1,7 +1,5 @@
-import { Position } from '@/types/position';
+import SnakeGameManager from '@/utils/snakeGame';
 import { useEffect, useRef } from 'react';
-
-const TILE_COUNT = 20;
 
 const SnakeGame = () => {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -29,164 +27,20 @@ const SnakeGame = () => {
       throw new Error('not found canvas element');
     }
 
-    const ctx = canvasRef.current.getContext('2d');
-
-    if (!ctx) {
-      throw new Error('not found canvas context');
-    }
-
-    let timeoutId: number | null = null;
-
+    const snakeGameManager = new SnakeGameManager(canvas);
     const containerSize = getContainerSize();
-
-    const tileSize = canvas.width / TILE_COUNT;
-
-    // TODO: 스타트 위치는 랜덤으로
-    let headX = 10;
-    let headY = 10;
-
-    const snakeParts: Position[] = [];
-    let tailLength = 2;
-
-    let appleX = 5;
-    let appleY = 5;
-
-    let inputXVelocity = 0;
-    let inputYVelocity = 0;
-
-    let xVelocity = 0;
-    let yVelocity = 0;
-
-    let score = 0;
-    let speed = 10;
-
-    canvas.width = containerSize.width;
-    canvas.height = containerSize.height;
-
-    const drawGame = () => {
-      xVelocity = inputXVelocity;
-      yVelocity = inputYVelocity;
-
-      changeSnakePosition();
-
-      if (isGameOver()) {
-        alert('GameOver !!!');
-        return;
-      }
-
-      clearScreen();
-
-      checkAppleCollision();
-      drawApple();
-      drawSnake();
-
-      if (score > 5) {
-        speed = 12;
-      }
-      if (score > 10) {
-        speed = 15;
-      }
-
-      timeoutId = setTimeout(drawGame, 1000 / speed);
-    };
-
-    const isGameOver = () => {
-      if (xVelocity === 0 && yVelocity === 0) {
-        return false;
-      }
-
-      if (headX < 0 || headY < 0 || headX === TILE_COUNT || headY === TILE_COUNT) {
-        return true;
-      }
-
-      for (const snakePart of snakeParts) {
-        if (snakePart.x === headX && snakePart.y === headY) {
-          return true;
-        }
-      }
-
-      return false;
-    };
-
-    const clearScreen = () => {
-      ctx.fillStyle = 'white';
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-    };
-
-    const drawSnake = () => {
-      ctx.fillStyle = 'green';
-      snakeParts.forEach((snakePart) => {
-        ctx.fillRect(snakePart.x * TILE_COUNT, snakePart.y * TILE_COUNT, tileSize, tileSize);
-      });
-
-      snakeParts.push({ x: headX, y: headY });
-      while (snakeParts.length > tailLength) {
-        snakeParts.shift();
-      }
-
-      ctx.fillStyle = 'orange';
-      ctx.fillRect(headX * TILE_COUNT, headY * TILE_COUNT, tileSize, tileSize);
-    };
-
-    const changeSnakePosition = () => {
-      headX = headX + xVelocity;
-      headY = headY + yVelocity;
-    };
-
-    const drawApple = () => {
-      ctx.fillStyle = 'red';
-      ctx.fillRect(appleX * TILE_COUNT, appleY * TILE_COUNT, tileSize, tileSize);
-    };
-
-    const checkAppleCollision = () => {
-      if (appleX === headX && appleY === headY) {
-        appleX = Math.floor(Math.random() * TILE_COUNT);
-        appleY = Math.floor(Math.random() * TILE_COUNT);
-        tailLength += 1;
-        score += 1;
-      }
-    };
-
-    const handleKeydown = (event: KeyboardEvent) => {
-      switch (event.key) {
-        case 'ArrowUp':
-          if (inputYVelocity === 1) break;
-          inputXVelocity = 0;
-          inputYVelocity = -1;
-          break;
-        case 'ArrowDown':
-          if (inputYVelocity === -1) break;
-          inputXVelocity = 0;
-          inputYVelocity = 1;
-          break;
-        case 'ArrowLeft':
-          if (inputXVelocity === 1) break;
-          inputXVelocity = -1;
-          inputYVelocity = 0;
-          break;
-        case 'ArrowRight':
-          if (inputXVelocity === -1) break;
-          inputXVelocity = 1;
-          inputYVelocity = 0;
-          break;
-        default:
-          break;
-      }
-    };
-
-    document.addEventListener('keydown', handleKeydown);
-
-    drawGame();
+    snakeGameManager.setCanvasSize(containerSize);
+    snakeGameManager.initialize();
+    snakeGameManager.drawGame();
 
     return () => {
-      if (timeoutId) clearTimeout(timeoutId);
-      document.removeEventListener('keydown', handleKeydown);
+      snakeGameManager.drawEnd();
     };
   }, []);
 
   return (
     <div ref={containerRef} className='h-full w-full'>
-      <canvas ref={canvasRef} className='border bg-black' />;
+      <canvas ref={canvasRef} className='border bg-black' />
     </div>
   );
 };
--- src/utils/snakeGame.ts
+++ src/utils/snakeGame.ts
@@ -1,8 +1,13 @@
 import { Position } from '@/types/position';
+import { Size } from '@/types/size';
 
 export const TILE_COUNT = 20;
 
-export default class SnakeGame {
+export default class SnakeGameManager {
+  #canvas: HTMLCanvasElement;
+  #canvasContext: CanvasRenderingContext2D;
+  #tileSize: number;
+
   #headPosition: Position;
   #applePosition: Position;
 
@@ -12,7 +17,16 @@ export default class SnakeGame {
   #score: number;
   #speed: number;
 
-  constructor() {
+  #timeoutId: number | null;
+
+  #snakeParts: Position[];
+  #snakeTailLength: number;
+
+  constructor(canvas: HTMLCanvasElement) {
+    this.#canvas = canvas;
+    this.#canvasContext = this.#getCanvasContext(canvas);
+    this.#tileSize = canvas.width / TILE_COUNT;
+
     this.#headPosition = this.#getRandomPosition();
     this.#applePosition = this.#getRandomPosition();
 
@@ -21,9 +35,164 @@ export default class SnakeGame {
 
     this.#score = 0;
     this.#speed = 10;
+
+    this.#timeoutId = null;
+
+    this.#snakeParts = [];
+    this.#snakeTailLength = 2;
   }
 
   #getRandomPosition() {
     return { x: Math.floor(Math.random() * TILE_COUNT), y: Math.floor(Math.random() * TILE_COUNT) };
+  }
+
+  #getCanvasContext(canvas: HTMLCanvasElement) {
+    const context = canvas.getContext('2d');
+
+    if (!context) {
+      throw new Error('Not Found Canvas 2D Context');
+    }
+
+    return context;
+  }
+
+  #changeSnakePosition() {
+    const headX = this.#headPosition.x + this.#velocity.x;
+    const headY = this.#headPosition.y + this.#velocity.y;
+    this.#headPosition = { x: headX, y: headY };
+  }
+
+  #isGameOver = () => {
+    if (this.#velocity.x === 0 && this.#velocity.y === 0) {
+      return false;
+    }
+
+    const { x: headX, y: headY } = { ...this.#headPosition };
+
+    if (headX < 0 || headY < 0 || headX === TILE_COUNT || headY === TILE_COUNT) {
+      return true;
+    }
+
+    for (const snakePart of this.#snakeParts) {
+      if (snakePart.x === headX && snakePart.y === headY) {
+        return true;
+      }
+    }
+
+    return false;
+  };
+
+  #clearScreen() {
+    this.#canvasContext.fillStyle = 'white';
+    this.#canvasContext.fillRect(0, 0, this.#canvas.width, this.#canvas.height);
+  }
+
+  #drawSnake() {
+    const convasContext = this.#canvasContext;
+    const snakeParts = this.#snakeParts;
+    const { x: headX, y: headY } = this.#headPosition;
+
+    convasContext.fillStyle = 'green';
+    snakeParts.forEach((snakePart) => {
+      convasContext.fillRect(snakePart.x * TILE_COUNT, snakePart.y * TILE_COUNT, this.#tileSize, this.#tileSize);
+    });
+
+    snakeParts.push({ x: headX, y: headY });
+    while (snakeParts.length > this.#snakeTailLength) {
+      snakeParts.shift();
+    }
+
+    convasContext.fillStyle = 'orange';
+    convasContext.fillRect(headX * TILE_COUNT, headY * TILE_COUNT, this.#tileSize, this.#tileSize);
+  }
+
+  #drawApple() {
+    this.#canvasContext.fillStyle = 'red';
+    this.#canvasContext.fillRect(
+      this.#applePosition.x * TILE_COUNT,
+      this.#applePosition.y * TILE_COUNT,
+      this.#tileSize,
+      this.#tileSize
+    );
+  }
+
+  #checkAppleCollision() {
+    if (this.#applePosition.x === this.#headPosition.x && this.#applePosition.y === this.#headPosition.y) {
+      this.#applePosition = this.#getRandomPosition();
+      this.#snakeTailLength += 1;
+      this.#score += 1;
+    }
+  }
+
+  #handleKeydown(event: KeyboardEvent) {
+    const { x: inputXVelocity, y: inputYVelocity } = this.#inputVelocity;
+    switch (event.key) {
+      case 'ArrowUp':
+        if (inputYVelocity === 1) break;
+        this.#inputVelocity = { x: 0, y: -1 };
+        break;
+      case 'ArrowDown':
+        if (inputYVelocity === -1) break;
+        this.#inputVelocity = { x: 0, y: 1 };
+        break;
+      case 'ArrowLeft':
+        if (inputXVelocity === 1) break;
+        this.#inputVelocity = { x: -1, y: 0 };
+        break;
+      case 'ArrowRight':
+        if (inputXVelocity === -1) break;
+        this.#inputVelocity = { x: 1, y: 0 };
+        break;
+      default:
+        break;
+    }
+  }
+
+  #addEventHandler() {
+    document.addEventListener('keydown', this.#handleKeydown.bind(this));
+  }
+
+  #removeEventHandler() {
+    document.removeEventListener('keydown', this.#handleKeydown.bind(this));
+  }
+
+  setCanvasSize({ width, height }: Size) {
+    this.#canvas.width = width;
+    this.#canvas.width = height;
+  }
+
+  initialize() {
+    this.#addEventHandler();
+  }
+
+  drawGame() {
+    this.#velocity = { ...this.#inputVelocity };
+
+    this.#changeSnakePosition();
+
+    if (this.#isGameOver()) {
+      alert('GameOver !!!');
+      return;
+    }
+
+    this.#clearScreen();
+
+    this.#checkAppleCollision();
+    this.#drawApple();
+    this.#drawSnake();
+
+    if (this.#score > 5) {
+      this.#speed = 12;
+    }
+    if (this.#score > 10) {
+      this.#speed = 15;
+    }
+
+    this.#timeoutId = setTimeout(this.drawGame.bind(this), 1000 / this.#speed);
+  }
+
+  drawEnd() {
+    if (this.#timeoutId) clearTimeout(this.#timeoutId);
+    this.#removeEventHandler();
   }
 }
