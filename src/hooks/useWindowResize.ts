dcb28e5d2bef97d1c12005eca2cc3b6cc375e3fa
minjongbaek <minjongbaek@gmail.com>
2024-05-10 17:30:28 +0900

    feat: 윈도우 리사이즈 이벤트를 핸들링하는 커스텀 훅 작성

---
+++ src/hooks/useWindowResize.ts
@@ -0,0 +1,29 @@
+import { WINDOW_MIN_HEIGHT, WINDOW_MIN_WIDTH } from '@/constants/resize';
+import { useEffect, useState } from 'react';
+
+const useWindowResize = () => {
+  const [isAvailableWindowSize, setIsAvailableWindowSize] = useState(false);
+
+  const resizeHandler = () => {
+    const { innerWidth: width, innerHeight: height } = window;
+
+    if (width <= WINDOW_MIN_WIDTH || height <= WINDOW_MIN_HEIGHT) {
+      setIsAvailableWindowSize(false);
+      return;
+    }
+
+    setIsAvailableWindowSize(true);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', resizeHandler);
+
+    return () => {
+      window.removeEventListener('resize', resizeHandler);
+    };
+  }, []);
+
+  return isAvailableWindowSize;
+};
+
+export default useWindowResize;
